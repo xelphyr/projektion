ac4015c50135e235361f86824d43d94d998c88fd
xelphyr <xelphyrnull@gmail.com>
2026-02-17 23:17:12 +0530

    setup db stuff

--- apps/slack-bot/src/listeners/commands/register.js
+++ apps/slack-bot/src/listeners/commands/register.js
@@ -1,6 +1,11 @@
-const registerCallback = async ({ ack, respond, logger }) => {
+import { addProject } from "@projektion/db/queries";
+
+const registerCallback = async ({ ack, respond, body, payload, logger }) => {
   try {
     await ack();
+    logger.info(payload)
+    logger.info(body)
+    //await addProject()
     await respond('Register INDEV');
   } catch (error) {
     logger.error(error);
